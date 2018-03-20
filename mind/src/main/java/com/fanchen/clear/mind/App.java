17009e67f81b42c607304fe6e97c775bca3ac121
fanchencopart <fan.chen@copart.com>
2018-03-20 17:20:01 -0500

    add practice poject

---
+++ mind/src/main/java/com/fanchen/clear/mind/App.java
@@ -0,0 +1,13 @@
+package com.fanchen.clear.mind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
