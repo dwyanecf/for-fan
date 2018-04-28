8490b21ebff4178ca69f1e96b0d4885535bf4142
fanchencopart <fan.chen@copart.com>
2018-04-28 17:37:30 -0500

    Three tree using recursion and queue

---
+++ mind/src/main/java/com/fanchen/clearmind/leetcode/tree/MaximumDepthofBinaryTree.java
@@ -0,0 +1,55 @@
+package com.fanchen.clearmind.leetcode.tree;
+
+import com.fanchen.clearmind.concept.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, find its maximum depth.
+ *
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its depth = 3.
+ */
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        return left > right ? left : right;
+    }
+
+    public int maxDepthq(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int count = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size > 0) {
+                TreeNode n = q.poll();
+                if (n.left != null) {
+                    q.offer(n.left);
+                }
+                if (n.right != null) {
+                    q.offer(n.right);
+                }
+                size--;
+            }
+            count++;
+        }
+        return count;
+    }
+}
