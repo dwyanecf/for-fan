65bb1b238da41ff0047f875d0c5e64bc502cdfaa
fanchencopart <fan.chen@copart.com>
2018-06-01 09:23:14 -0500

    two pointer finish easy

---
+++ mind/src/main/java/com/fanchen/clearmind/leetcode/TwoPointer/TwoSumIIInputarrayissorted.java
@@ -0,0 +1,40 @@
+package com.fanchen.clearmind.leetcode.TwoPointer;
+
+/**
+ * Given an array of integers that is already sorted in ascending order,
+ * find two numbers such that they add up to a specific target number.
+ *
+ * The function twoSum should return indices of the two numbers such that they add up to the target,
+ * where index1 must be less than index2.
+ *
+ * Note:
+ *
+ * Your returned answers (both index1 and index2) are not zero-based.
+ * You may assume that each input would have exactly one solution and you may not use the same element twice.
+ * Example:
+ *
+ * Input: numbers = [2,7,11,15], target = 9
+ * Output: [1,2]
+ * Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+ */
+public class TwoSumIIInputarrayissorted {
+    public int[] twoSum(int[] numbers, int target) {
+        if(numbers == null) return new int[0];
+        int lo = 0;
+        int hi = numbers.length-1;
+        int[] res =  new int[2];
+        while (lo < hi){
+            int sum = numbers[lo] + numbers[hi];
+            if(sum == target){
+                res[0] = lo + 1;
+                res[1] = hi + 1;
+                return res;
+            }else if( sum < target){
+                lo++;
+            }else{
+                hi--;
+            }
+        }
+        return res;
+    }
+}
