46a7ac11e031e864bc755def137df766424ed483
fanchencopart <fan.chen@copart.com>
2018-06-25 09:31:42 -0500

    do my own job

---
+++ mind/src/main/java/com/fanchen/clearmind/leetcode/map/medium/GroupAnagrams.java
@@ -0,0 +1,38 @@
+package com.fanchen.clearmind.leetcode.map.medium;
+
+import java.util.*;
+
+/**
+ * Given an array of strings, group anagrams together.
+ *
+ * Example:
+ *
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * Note:
+ *
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+        if (strs == null || strs.length == 0) return res;
+        for (String word : strs) {
+            char[] c = word.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
