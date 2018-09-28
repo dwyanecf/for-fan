a43dc84ac4aee095853178b50cdad2eba7765def
fanchencopart <fan.chen@copart.com>
2018-09-28 17:00:12 -0500

    finished dp easy

---
+++ mind/src/main/java/com/fanchen/clearmind/GroupSolve/DP/NumArray.java
@@ -0,0 +1,38 @@
+package com.fanchen.clearmind.GroupSolve.DP;
+
+/**
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ *
+ * Example:
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ *
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * Note:
+ * You may assume that the array does not change.
+ * There are many calls to sumRange function.
+ */
+
+
+/**
+ * ["NumArray"]
+ * [[[]]]
+ */
+public class NumArray {
+    public int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length];
+        if(nums==null || nums.length==0) return;
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+
+    }
+
+    public int sumRange(int i, int j) {
+        return i==0? sum[j]: sum[j] - sum[i-1];
+    }
+}
---
+++ mind/src/main/java/com/fanchen/clearmind/GroupSolve/DP/PaintFence.java
@@ -0,0 +1,41 @@
+package com.fanchen.clearmind.GroupSolve.DP;
+
+/**
+ * There is a fence with n posts, each post can be painted with one of the k colors.
+ * <p>
+ * You have to paint all the posts such that no more than two adjacent fence posts have the same color.
+ * <p>
+ * Return the total number of ways you can paint the fence.
+ * <p>
+ * Note:
+ * n and k are non-negative integers.
+ * <p>
+ * Example:
+ * <p>
+ * Input: n = 3, k = 2
+ * Output: 6
+ * Explanation: Take c1 as color 1, c2 as color 2. All possible ways are:
+ * <p>
+ * post1  post2  post3
+ * -----      -----  -----  -----
+ * 1         c1     c1     c2
+ * 2         c1     c2     c1
+ * 3         c1     c2     c2
+ * 4         c2     c1     c1
+ * 5         c2     c1     c2
+ * 6         c2     c2     c1
+ */
+public class PaintFence {
+    public int numWays(int n, int k) {
+        if (n == 0) return 0;
+        if (n == 1) return k;
+        int diff = k * (k - 1);
+        int same = k;
+        for (int i = 2; i < n; i++) {
+            int temp = diff;
+            diff = (diff + same) * (k - 1);
+            same = temp;
+        }
+        return same + diff;
+    }
+}
