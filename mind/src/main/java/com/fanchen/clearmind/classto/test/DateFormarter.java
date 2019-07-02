b709d6a662fca1afe479d839b3bb27819b73cb6a
fanchencopart <fan.chen@copart.com>
2019-07-02 14:30:58 -0500

    failed at amazon

--- mind/src/main/java/com/fanchen/clearmind/classto/test/DateFormarter.java
+++ mind/src/main/java/com/fanchen/clearmind/classto/test/DateFormarter.java
@@ -1,5 +1,6 @@
 package com.fanchen.clearmind.classto.test;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,15 +12,33 @@ public class DateFormarter {
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-	public static String format(String date, String pattern) throws ParseException {
-		TimeZone tz = TimeZone.getDefault();
+	public static String format1(String date, String pattern) throws ParseException {
+		TimeZone tz = TimeZone.getTimeZone("EST");
+		System.out.println(tz.getDisplayName());
 		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
 		Date date1 = sdf.parse(date);
-		return DateFormatUtils.format(date1, pattern, tz);
+		System.out.println("Local time: "+ date1);
+		System.out.println(DateFormatUtils.format(date1, pattern, tz));
+		return "";
 	}
 
 	public static void main(String[] args) throws ParseException {
-		System.out.println(format("2019-06-03T23:45:00.000Z", "dd.MM.yyyy"));
-		System.out.println(format("2019-06-03T00:00:00.000Z", "dd.MM.yyyy"));
+//"dd.MM.yyyy"
+		System.out.println(format1("2019-06-25T04:26:22.000Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+		System.out.println("~~~~~~~~~~~~~~~~~");
+		System.out.println(format("2019-06-25T06:27:41.000Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
 	}
+
+	public static String format(String date, String pattern) throws ParseException {
+		DateFormat utcFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+		utcFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		Date thisDate = utcFormat.parse(date);
+		System.out.println("Local time: "+ thisDate);
+		DateFormat pstFormat = new SimpleDateFormat(pattern);
+		pstFormat.setTimeZone(TimeZone.getTimeZone("CET"));
+		System.out.println(pstFormat.format(thisDate));
+
+		return "";
+	}
+
 }
---
+++ mind/src/main/java/com/fanchen/clearmind/leetcode/ama/TwoSumII.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2019 Copart, Inc. All rights reserved.
+ */
+package com.fanchen.clearmind.leetcode.ama;
+
+/**
+ * @author fachen
+ *
+ */
+public class TwoSumII {
+	
+}
