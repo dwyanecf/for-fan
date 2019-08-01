5b31bfb8975b3958400485001313a58750319e0c
fanchencopart <fan.chen@copart.com>
2019-08-01 09:45:24 -0500

    first day done

---
+++ mind/src/main/java/com/fanchen/clearmind/concept/start/point/LetterTilePossibilities.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 Copart, Inc. All rights reserved.
+ */
+package com.fanchen.clearmind.concept.start.point;
+
+/**
+ * @author fachen You have a set of tiles, where each tile has one letter
+ *         tiles[i] printed on it. Return the number of possible non-empty
+ *         sequences of letters you can make.
+ * 
+ * 
+ * 
+ *         Example 1:
+ * 
+ *         Input: "AAB" Output: 8 Explanation: The possible sequences are "A",
+ *         "B", "AA", "AB", "BA", "AAB", "ABA", "BAA". Example 2:
+ * 
+ *         Input: "AAABBC" Output: 188
+ * 
+ * 
+ *         Note:
+ * 
+ *         1 <= tiles.length <= 7 tiles consists of uppercase English letters.
+ * 
+ */
+public class LetterTilePossibilities {
+	public int numTilePossibilities(String tiles) {
+		int[] table = new int[26];
+		for (char c : tiles.toCharArray()) {
+			table[c - 'A']++;
+		}
+		return recursion(table);
+
+	}
+
+	/**
+	 * @param table
+	 * @return
+	 */
+	private int recursion(int[] table) {
+		int sum = 0;
+		for (int i = 0; i < 26; i++) {
+			if (table[i] == 0)
+				continue;
+			sum++;
+			table[i]--;
+			sum += recursion(table);
+			table[i]++;
+		}
+		return sum;
+	}
+}
---
+++ mind/src/main/java/com/fanchen/clearmind/concept/start/point/OccurrencesAfterBigram.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2019 Copart, Inc. All rights reserved.
+ */
+package com.fanchen.clearmind.concept.start.point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given words first and second, consider occurrences in some text of the form
+ * "first second third", where second comes immediately after first, and third
+ * comes immediately after second.
+ * 
+ * For each such occurrence, add "third" to the answer, and return the answer.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: text = "alice is a good girl she is a good student", first = "a",
+ * second = "good" Output: ["girl","student"] Example 2:
+ * 
+ * Input: text = "we will we will rock you", first = "we", second = "will"
+ * Output: ["we","rock"]
+ * 
+ * 
+ * Note:
+ * 
+ * 1 <= text.length <= 1000 text consists of space separated words, where each
+ * word consists of lowercase English letters. 1 <= first.length, second.length
+ * <= 10 first and second consist of lowercase English letters.
+ * 
+ * @author fachen
+ *
+ */
+public class OccurrencesAfterBigram {
+	public String[] findOcurrences(String text, String first, String second) {
+		List<String> res = new ArrayList<>();
+		String[] words = text.split(" ");
+		int n = words.length;
+		int i = 0;
+		while (i < n - 1) {
+			if (words[i].equals(first) && words[i + 1].equals(second)) {
+				if (i + 2 < n) {
+					res.add(words[i + 2]);
+				}
+			}
+			i++;
+		}
+		return res.toArray(new String[0]);
+
+	}
+
+}
