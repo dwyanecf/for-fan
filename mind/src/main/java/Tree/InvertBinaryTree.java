08567e5172933cdfbf072df857566b51d8999a02
fanchencopart <fan.chen@copart.com>
2019-08-13 16:58:44 -0500

    work on trees

---
+++ mind/src/main/java/Tree/InvertBinaryTree.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2019 Copart, Inc. All rights reserved.
+ */
+package Tree;
+
+import com.fanchen.clearmind.concept.TreeNode;
+
+/**
+ * @author fachen
+ *
+ */
+
+/**
+ * Invert a binary tree.
+	
+	Example:
+	
+	Input:
+	
+	     4
+	   /   \
+	  2     7
+	 / \   / \
+	1   3 6   9
+	Output:
+	
+	     4
+	   /   \
+	  7     2
+	 / \   / \
+	9   6 3   1
+	Trivia:
+	This problem was inspired by this original tweet by Max Howell:
+	
+	Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+ * @author fachen
+ *
+ */
+public class InvertBinaryTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null)
+			return null;
+		if (root.left != null || root.right != null) {
+			TreeNode temp = root.left;
+			root.left = root.right;
+			root.right = temp;
+		}
+		invertTree(root.left);
+		invertTree(root.right);
+
+		return root;
+	}
+}
---
+++ mind/src/main/java/Tree/SortedArrayToBinaryTree.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2019 Copart, Inc. All rights reserved.
+ */
+package Tree;
+
+import com.fanchen.clearmind.concept.TreeNode;
+
+/**
+ * @author fachen
+ *
+ *
+ *         Given an array where elements are sorted in ascending order, convert
+ *         it to a height balanced BST.
+ * 
+ *         For this problem, a height-balanced binary tree is defined as a
+ *         binary tree in which the depth of the two subtrees of every node
+ *         never differ by more than 1.
+ * 
+ *         Example:
+ * 
+ *         Given the sorted array: [-10,-3,0,5,9],
+ * 
+ *         One possible answer is: [0,-3,9,-10,null,5], which represents the
+ *         following height balanced BST:
+ * 
+ *         0 / \ -3 9 / / -10 5
+ */
+public class SortedArrayToBinaryTree {
+	public TreeNode sortedArrayToBST(int[] nums) {
+		return helper(nums, 0, nums.length);
+	}
+
+	public TreeNode helper(int[] nums, int l, int r) {
+		if (l >= r)
+			return null;
+		int mid = l + (r - l) / 2;
+		TreeNode node = new TreeNode(nums[mid]);
+		node.left = helper(nums, l, mid);
+		node.right = helper(nums, mid + 1, r);
+		return node;
+	}
+}
