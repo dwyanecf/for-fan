60f653c864e81e85d2c7efef87f575f4b7c82401
fanchencopart <fan.chen@copart.com>
2019-08-15 16:43:46 -0500

    solve trees

---
+++ mind/src/main/java/com/fanchen/clearmind/leetcode/tree/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019 Copart, Inc. All rights reserved.
+ */
+package com.fanchen.clearmind.leetcode.tree;
+
+import com.fanchen.clearmind.concept.TreeNode;
+
+/**
+ * @author fachen
+ *
+ */
+public class BinaryTreeMaximumPathSum {
+	
+	public int max = Integer.MIN_VALUE;
+
+	public int maxPathSum(TreeNode root) {
+		helper(root);
+		return max;
+	}
+
+	/**
+	 * @param root
+	 */
+	private int helper(TreeNode root) {
+		if (root == null)
+			return 0;
+		int left = Math.max(helper(root.left), 0);
+		int right = Math.max(0, helper(root.right));
+		max = Math.max(max, root.val + left + right);
+		return root.val + Math.max(left, right);
+
+	}
+	
+}
