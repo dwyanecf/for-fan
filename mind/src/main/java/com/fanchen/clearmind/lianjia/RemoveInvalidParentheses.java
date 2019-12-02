745aacabd3bde02f06f713804d27b7a80b1904c1
fanchencopart <fan.chen@copart.com>
2019-12-02 16:33:16 -0600

    lets do it:

--- mind/src/main/java/com/fanchen/clearmind/lianjia/RemoveInvalidParentheses.java
+++ mind/src/main/java/com/fanchen/clearmind/lianjia/RemoveInvalidParentheses.java
@@ -4,48 +4,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Remove the minimum number of invalid parentheses in order to make the input string valid.
- * Return all possible results.
+ * Remove the minimum number of invalid parentheses in order to make the input
+ * string valid. Return all possible results.
  *
- * Note: The input string may contain letters other than the parentheses ( and ).
+ * Note: The input string may contain letters other than the parentheses ( and
+ * ).
  *
  * Example 1:
  *
- * Input: "()())()"
- * Output: ["()()()", "(())()"]
- * Example 2:
+ * Input: "()())()" Output: ["()()()", "(())()"] Example 2:
  *
- * Input: "(a)())()"
- * Output: ["(a)()()", "(a())()"]
- * Example 3:
+ * Input: "(a)())()" Output: ["(a)()()", "(a())()"] Example 3:
  *
- * Input: ")("
- * Output: [""]
+ * Input: ")(" Output: [""]
  */
 public class RemoveInvalidParentheses {
-    public List<String> removeInvalidParentheses(String s) {
-        List<String> list = new ArrayList<>();
-        helper(list, s, new char[]{'(', ')'}, 0, 0);
-        return list;
-    }
 
-    private void helper(List<String> list, String s, char[] brackets, int indexI, int indexJ) {
-        for (int count = 0, i = indexI; i < s.length(); i++) {
-            if (s.charAt(i) == brackets[0]) count++;
-            if (s.charAt(i) == brackets[1]) count--;
-            if (count >= 0) continue;
-            for (int j = indexJ; j <= i; j++) {
-                if (s.charAt(j) == brackets[1] && (j == indexJ || s.charAt(j - 1) != brackets[1])){
-                    helper(list, s.substring(0,j) + s.substring(j+1, s.length()), brackets, i, j);
-                }
-            }
-            return;
-        }
-        String reversedS = new StringBuilder(s).reverse().toString();
-        if (brackets[0] == '(') {
-            helper(list, reversedS, new char[]{')', '('}, 0, 0);
-        } else {
-            list.add(reversedS);
-        }
-    }
+	public List<String> removeInvalidParentheses(String s) {
+		List<String> res = new ArrayList<>();
+		helper(res, s, 0, 0, new char[] { '(', ')' });
+		return res;
+	}
+
+	/**
+	 * @param res
+	 * @param s
+	 * @param i
+	 * @param j
+	 * @param cs
+	 */
+	private void helper(List<String> res, String s, int last_i, int last_j, char[] cs) {
+		int count = 0;
+		for (int i = last_i; i < s.length(); i++) {
+			if (s.charAt(i) == cs[0])
+				count++;
+			if (s.charAt(i) == cs[1])
+				count--;
+			if (count >= 0)
+				continue;
+
+			for (int j = last_j; j <= i; j++) {
+				if (s.charAt(j) == cs[1] && (j == last_j || s.charAt(j - 1) != cs[1])) {
+					helper(res, s.substring(0, j) + s.substring(j + 1, s.length()), i, j, cs);
+				}
+			}
+			return;
+
+		}
+		String reversed = new StringBuilder(s).reverse().toString();
+		if (cs[0] == '(') {
+			helper(res, reversed, 0, 0, new char[] { ')', '(' });
+		} else {
+			res.add(reversed);
+		}
+
+	}
 }
---
+++ mind/src/main/java/com/fanchen/clearmind/lianjia/SubarraySumEqualsK.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2019 Copart, Inc. All rights reserved.
+ */
+package com.fanchen.clearmind.lianjia;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author fachen
+ * 
+ * 
+ *         Given an array of integers and an integer k, you need to find the
+ *         total number of continuous subarrays whose sum equals to k.
+ * 
+ *         Example 1: Input:nums = [1,1,1], k = 2 Output: 2
+ * 
+ *         Note: The length of the array is in range [1, 20,000]. The range of
+ *         numbers in the array is [-1000, 1000] and the range of the integer k
+ *         is [-1e7, 1e7].
+ *
+ */
+public class SubarraySumEqualsK {
+	public int subarraySum(int[] nums, int k) {
+		Map<Integer, Integer> preSum = new HashMap<>();
+		preSum.put(0, 1);
+		int ans = 0;
+		int sum = 0;
+		for (int n : nums) {
+			sum += n;
+			if (preSum.containsKey(sum - k)) {
+				ans += preSum.get(sum - k);
+			}
+			preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+		}
+		return ans;
+	}
+}
