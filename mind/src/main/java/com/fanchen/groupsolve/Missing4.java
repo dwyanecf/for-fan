f9b738c5b3815f22bd9fbd99aa2ac3a4a4b8ef9d
fanchencopart <fan.chen@copart.com>
2020-01-15 10:41:42 -0600

    fix missing 4

--- mind/src/main/java/com/fanchen/groupsolve/Missing4.java
+++ mind/src/main/java/com/fanchen/groupsolve/Missing4.java
@@ -3,6 +3,8 @@
  */
 package com.fanchen.groupsolve;
 
+import java.util.Arrays;
+
 /**
  * @author fachen
  *
@@ -10,49 +12,34 @@ package com.fanchen.groupsolve;
 public class Missing4 {
 
 	public static void missing4(int[] arr) {
-		// To keep track of 4 possible numbers
-		// greater than length of input array
-		// In Java, helper is automatically
-		// initialized as 0.
 		int[] helper = new int[4];
+		Arrays.fill(helper, 1);
 
-		// Traverse the input array and mark
-		// visited elements either by marking
-		// them as negative in arr[] or in
-		// helper[].
 		for (int i = 0; i < arr.length; i++) {
-			int temp = Math.abs(arr[i]);
+			int val = Math.abs(arr[i]) - 1;
 
-			// If element is smaller than or
-			// equal to length, mark its
-			// presence in arr[]
-			if (temp <= arr.length)
-				arr[temp - 1] *= -1;
+			if (val < arr.length)
+				arr[val] = -arr[val];
 
-			// Mark presence in helper[]
-			else if (temp > arr.length) {
-				if (temp % arr.length != 0)
-					helper[temp % arr.length - 1] = -1;
-				else
-					helper[(temp % arr.length) + arr.length - 1] = -1;
+			else {
+				helper[val - arr.length] = -1;
+
 			}
 		}
 
-		// Print all those elements whose presence
-		// is not marked.
 		for (int i = 0; i < arr.length; i++)
 			if (arr[i] > 0)
 				System.out.print(i + 1 + " ");
 		for (int i = 0; i < helper.length; i++)
-			if (helper[i] >= 0)
+			if (helper[i] > 0)
 				System.out.print(arr.length + i + 1 + " ");
 
-		return ;
+		return;
 	}
 
 	// Driver code
 	public static void main(String[] args) {
-		int[] arr = { 1, 7, 3, 12, 5, 10, 8, 4, 9 };
+		int[] arr = { 1, 5 };
 		missing4(arr);
 	}
 }
