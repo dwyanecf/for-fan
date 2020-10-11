e4013bd343cf3971358d6365c96eefdc58a0ae68
Fan Chen <Fan.Chen@copart.com>
2020-10-11 18:25:16 -0500

    review old and new problems

---
+++ mind/src/main/java/com/fanchen/clearmind/leetcode/ama/MusicPairs.java
@@ -0,0 +1,24 @@
+package com.fanchen.clearmind.leetcode.ama;
+
+public class MusicPairs {
+
+	/**
+	 * Refer to
+	 * https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/submissions/
+	 * 
+	 * @param time
+	 * @return
+	 */
+	public int numPairsDivisibleBy60(int[] time) {
+		int[] count = new int[60];
+		int res = 0;
+		for (int t : time) {
+			int remain = t % 60;
+			int left = (60 - remain) % 60;
+			res += count[left];
+			count[remain]++;
+
+		}
+		return res;
+	}
+}
